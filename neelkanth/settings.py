ad7ab6cb4203ba8196253c8cd783d9533a84c78b
akash singh <36296166+skynolimits247@users.noreply.github.com>
2018-07-08 18:59:14 +0530

    Update settings.py

--- neelkanth/settings.py
+++ neelkanth/settings.py
@@ -29,8 +29,8 @@ ALLOWED_HOSTS = []
 
 ALLOWED_HOSTS = []
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='neelkantha.apartment@gmail.com'
-EMAIL_HOST_PASSWORD='Neelkantha@123'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
 EMAI_PORT='587'
 EMAIL_USE_TLS='True'
 
